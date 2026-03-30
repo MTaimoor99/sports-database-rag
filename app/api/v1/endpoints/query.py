08af12b10be8d151997f0dd8126c5ed97bf8e232
Taimoor <taimoor992002@gmail.com>
2026-03-30 12:32:37 +0500

    accepted user input from frontend and got response from backend

--- app/api/v1/endpoints/query.py
+++ app/api/v1/endpoints/query.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from app.models.query import QueryResponse,QueryRequest
+
 router = APIRouter()
 
-class QueryRequest(BaseModel):
-    query: str
-
-class QueryResponse(BaseModel):
-    answer: str
-
-@router.post("/query", response_model=QueryResponse)
+@router.post("/process-user-query", response_model=QueryResponse)
 async def handle_query(request: QueryRequest):
     # LangGraph agent will be wired in here later
     return QueryResponse(answer=f"Received: {request.query}")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints.query import router as query_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Sports Database RAG")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(query_router, prefix="/api/v1")
 
