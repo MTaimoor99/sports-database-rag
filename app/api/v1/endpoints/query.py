726a56b679567d5c6414f3c2bea514a5c00aa15b
Taimoor <taimoor992002@gmail.com>
2026-03-31 11:11:02 +0500

    addressed bugs that happened because of changing architecture

--- app/api/v1/endpoints/query.py
+++ app/api/v1/endpoints/query.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 from app.models.request_response.query import QueryResponse,QueryRequest
-from services.user_chat_service import UserChatService
+from app.services.user_chat_service import UserChatService
 from app.providers.user_chat.user_chat_api_providers import get_user_chat_service
 
 router = APIRouter()
@@ -11,5 +11,5 @@ router = APIRouter()
 async def handle_query(
     request: QueryRequest,
     user_chat_service: UserChatService = Depends(get_user_chat_service)):
-    answer = user_chat_service.process_user_query(request.query)
+    answer = await user_chat_service.process_user_query(request.query)
     return QueryResponse(answer=f"Received: {answer}")
